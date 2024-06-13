f239c6bfbb95bec15f06df499cdd221344beef4c
832022 <hotsuma_kawashima@densan-s.co.jp>
2024-06-13 10:46:45 +0900

    feat: add main.go

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"gopkg.in/elazarl/goproxy.v1"
+)
+
+func main() {
+	proxy := goproxy.NewProxyHttpServer()
+	proxy.Verbose = true
+	log.Fatal(http.ListenAndServe(":8080", proxy))
+}
